8809af6cac4ed3fbc91f50001bc1d7c5b10bbcee
Ragini340 <ragini31bxr@gmail.com>
2022-11-20 20:04:29 +0530

    Add project files.

---
+++ VarPOC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice
+{
+    public class VarPOC
+    {
+        static void Main(string[] args)
+        {
+            var a= "Ragini";
+            var roll = 340;
+            Console.WriteLine("Type of 'a' is : {0}", a.GetType());
+            Console.WriteLine("Type of 'b' is : {0} ", roll.GetType());
+
+            var xs = new List <string>();
+            xs.Add("Ragini");
+            xs.Add("Ragini Ragini");
+            xs.Add("Ragini Singh");
+            xs.Add("R Singh");
+            foreach (var s in xs)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
---
+++ WhereClausePOC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice
+{
+   public class WhereClausePOC
+    {
+        public static void Main()
+        {
+          List<string> fruits = new List<string> { "apple", "passionfruit", "banana", "mango","orange", "blueberry", "grape", "strawberry" };
+
+            IEnumerable<string> query = fruits.Where(fruit => fruit.Length < 6);
+
+            foreach (string fruit in query)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
