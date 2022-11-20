3fbcbf32155d2dd2f457b151099c5dde44f88511
Ragini340 <ragini31bxr@gmail.com>
2022-11-20 20:55:19 +0530

    ToUpperMethodPOC

---
+++ ToUpperMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice
+{
+    public class ToUpperMethod
+    { 
+        public static void Main()
+        {
+            string str = "ragini";
+            string upperstr = str.ToUpper();
+            Console.WriteLine(upperstr);
+
+        }
+    }
+}
